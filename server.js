ff54dd832d7082d4a98423fdfa961cb348dbe102
Omer <kirkanomer@yandex.com>
2023-04-07 14:14:23 +0300

    DB CONNECTED

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-
+const db = require("./dbConn");
 app.use(cors());
+app.use(express.json());
 
-const users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Doe" },
-  { id: 3, name: "John Smith" },
-  { id: 4, name: "Jane Smith" }
-];
+db.connect((err) => {
+  if (err) {
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log("Connection established");
+});
 
-app.get("/", (req, res) => {
-    res.json(users);
+app.get("/getusers", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(result.rows);
+  });
+});
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+      res.send("Values inserted");
+    }
+  );
 });
 
 app.listen(port, () => {
